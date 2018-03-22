d1483abaa77285348b7ab42ca6dbf36dfff4da45
spidersss <604815982@qq.com>
2018-03-22 14:07:00 +0800

    add multi_points follow_path

--- driverless_test/src/upper_control.cpp
+++ driverless_test/src/upper_control.cpp
@@ -13,12 +13,31 @@
 #define RAD2DEG
 driverless_test::control_data control_data;
 serial_node::serial_send sendmsg;
-float t_yaw = 0.0;
-int stop_flag = 0;
-int visit_flag = 0;
-float add_part = 25;
-float pre_steer = 90;
-int times = 0;
+
+double gpsYawCorrector(double gpsYaw)
+{
+	//if(gpsYaw > 270) return gpsYaw - 360;
+	//else return gpsYaw;
+	return gpsYaw;
+}
+double tarYawCreator(double endlat, double endlon, double gpslat, double gpslon)
+{
+	double t_yaw = atan2((cos(endlat)*(endlon - gpslon)),(endlat - gpslat))*180/pi;
+	if(t_yaw < 0) t_yaw = t_yaw + 360;
+	//if(t_yaw > 90 ) t_yaw = 180 - t_yaw;
+	return t_yaw;
+}
+double distanceCal(double beglat, double beglon, double endlat, double endlon, double gpslat, double gpslon)
+{
+	double func_k = (endlat -beglat) / (endlon- beglon);//直线的斜率：y:lat x:lon
+	double func_b = endlat - func_k * endlon;
+	return (func_k * gpslon - gpslat + func_b) /(sqrt((func_k * func_k)+1))*100000;
+}
+double distance(double a1, double b1, double a2, double b2)
+{
+	double t = ((a1-a2)*100000);
+	return sqrt(pow((a1-a2)*100000, 2) + pow((b1-b2)*100000,2));
+}
 class gpsHandler
 {
 public:
@@ -30,15 +49,36 @@ public:
 		
 		f = boost::bind(&gpsHandler::callbackConfig, this, _1, _2);
 	    server.setCallback(f);
-	    
+	    /*
 	    ros::param::get("~endlat", endlat);
 	    ros::param::get("~endlon", endlon);
 	    ros::param::get("~beglat", beglat);
 	    ros::param::get("~beglon", beglon);
+	    */
 	    ros::param::get("~speed", speed);
 	    
 	    pid_init(pid_ctrl);
 	    pid_set_frequency(pid_ctrl, 20);
+	    
+	    control_data.speed = speed;
+		control_data.steer = 0;
+		control_data.power = 1;
+		
+		count = 0;
+		t_yaw = 0.0;
+	    enable_flag = 0;
+	    disToend = 3.0;
+		
+		endlat[0] = 31.88685107;
+		endlon[0] = 118.81001789;
+		endturn[0] = 270;
+		endlat[1] = 31.88666064;
+		endlon[1] = 118.80938060;
+		endturn[1] = 180.0;
+		endlat[2] = 33.88722013;
+		endlon[2] = 120.81050525;
+		endturn[2] = 270.0;
+		
 	}
 	void callbackConfig(driverless_test::driverless_Config &config, uint32_t level)
 	{
@@ -53,54 +93,39 @@ public:
 	void controlCallback(const driverless_test::gps_data::ConstPtr& gps_msg)
 	{
 	  
-	  ROS_INFO("endlat:%f\tendlon:%f\t", endlat, endlon);
-	  if(times == 0){
-		control_data.speed = speed;
-		control_data.steer = 0;
-		control_data.power = 1;
-		func_k = (endlat -beglat) / (endlon- beglon);//直线的斜率：y:lat x:lon
-	  	func_b = endlat - func_k * endlon;
-		times = 1;	
-	  }
-	  t_yaw = atan2((cos(endlat)*(endlon - gps_msg->lon)),(endlat - gps_msg->lat))*180/pi;
-	  
-	  if(t_yaw < -90) t_yaw = t_yaw + 180;
-	  
-	  if(t_yaw > 90 ) t_yaw = 180 - t_yaw;
-	  ROS_INFO("t_yaw: %f\tdisToend: %f", t_yaw,disToend);
-	  pre_steer = control_data.steer;
-	  //yaw_error = t_yaw - gps_msg->yaw;
-	  yaw_now = gps_msg->yaw;
-	  if(gps_msg->yaw > 270) yaw_now = gps_msg->yaw - 360;
+	  ROS_INFO("%dendlat:%f\tendlon:%f\t", count, endlat[count], endlon[count]);
+	  t_yaw = tarYawCreator(endlat[count], endlon[count], gps_msg->lat, gps_msg->lon);
+	  yaw_now = gpsYawCorrector(gps_msg->yaw);
 	  yaw_error = t_yaw - yaw_now;
 	  if(yaw_error < 2 && yaw_error > -2) pid_reset_integral(pid_ctrl);
-	  dis_error = (func_k * gps_msg->lon - gps_msg->lat + func_b) /(sqrt((func_k * func_k)+1))*100000;
-	  if(times > 0 ){
-		//control_data.steer = pid_process(pid_ctrl, yaw_error) + 90 - 
-		//(gps_msg->lon - _endlon)*100000;
-		control_data.steer = yaw_error*(-5.0) + 90 + dis_error* 1.0;
-		ROS_INFO("yaw_error:%f\tdis_error:%f", yaw_error, dis_error);
-		if(control_data.steer < 60) control_data.steer = 60;
-		if(control_data.steer > 120) control_data.steer = 120;
+	  
+	  
+	  //dis_error = distanceCal(beglat, beglon, endlat, endlon, gps_msg->lat, gps_msg->lon);
+	  //control_data.steer = pid_process(pid_ctrl, yaw_error) + 90 - 
+	  //(gps_msg->lon - _endlon)*100000;
+	  //control_data.steer = yaw_error*(-5.0) + 90 + dis_error* 1.0;
+	  
+	  
+	  control_data.steer = yaw_error*(-5.0) + 90;
+	  ROS_INFO("yaw_error:%f\tdis_error:%f", yaw_error, dis_error);
+	  if(control_data.steer < 60) control_data.steer = 60;
+	  if(control_data.steer > 120) control_data.steer = 120;
+	  ROS_INFO("t_yaw: %f\tnowYaw%f\tdisToend: %f", t_yaw,yaw_now, disToend);	
+	  
+	  disToend = distance(gps_msg->lat, gps_msg->lon, endlat[count], endlon[count]);
+	  if( disToend < 1 && !enable_flag){
+		enable_flag = 1;
 	  }
-	  /*
-	  if(control_data.steer == 0){
-		if(visit_flag == 1) {visit_flag = 0;}
-		else{
-			if(pre_steer > control_data.steer) control_data.steer = control_data.steer - add_part;
-			else if(pre_steer < control_data.steer) control_data.steer = control_data.steer + add_part;
-			else;
-			visit_flag = 1;
-		}
-	  }
-	  */		
-	  /*if( disToend < 1 ){
-		stop_flag = 1;
-	  }*/
-	  if(stop_flag) {
-		control_data.speed = 0;
-		control_data.steer = 0;
-		control_data.power = 0;
+	  if(enable_flag){ 
+	    if(fabs(gps_msg->yaw - endturn[count]) < 5) {
+	      control_data.steer = 90;
+		  enable_flag = 0;
+	  	  count ++;
+	    }
+	    else{
+	  	  if(endturn[count] > 0) control_data.steer = 60;
+		  else control_data.steer = 120;
+	    }
 	  }
 	  control_pub.publish(control_data);
 	}
@@ -112,17 +137,18 @@ protected:
     dynamic_reconfigure::Server<driverless_test::driverless_Config>::CallbackType f;
 	ros::Publisher send_pub;
     pid_ctrl_t* pid_ctrl = new(pid_ctrl_t);
-    double endlat;
-    double endlon;
-    double beglat;
-    double beglon;
+    double endlat[3];
+    double endlon[3];
+    double endturn[3];
+    //double beglat, beglon;
     int speed;
     double yaw_error;
     double dis_error;
     double disToend;
     double yaw_now;	
-    double func_k;
-    double func_b;
+    int count;
+    float t_yaw;
+	int enable_flag;
     
 };
 int main(int argc, char **argv)
